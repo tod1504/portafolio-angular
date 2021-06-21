3f00990f9f82cb3c263cc357788587b38105f0b7
Oscar <lopezceballososcarandres@gmail.com>
2021-06-20 20:14:50 -0700

    seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'portafolio', component: PortafolioComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'portafolio' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
